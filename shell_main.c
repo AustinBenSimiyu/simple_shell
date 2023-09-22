67c15ab4d49a2adf12c7255b29b7be02e50b0b9f
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2023-09-22 23:11:30 +0300

    ch

--- shell_main.c
+++ shell_main.c
@@ -7,8 +7,8 @@
 
 /**
  * main - the main shell code
- * @argc: number of arguments passed
- * @argv: program arguments to be parsed
+ * @ac: number of arguments passed
+ * @av: program arguments to be parsed
  *
  * applies the functions in utils and helpers
  * implements EOF
@@ -16,14 +16,14 @@
  * Return: 0 on success
  */
 
-int main(int argc __attribute__((unused)), char **argv)
+int main(int ac __attribute__((unused)), char **av)
 {
-	char **current_command = NULL;
-	int i, type_command = 0;
+	char **nowcmd = NULL;
+	int a = 0, entercmd = 0;
 	size_t n = 0;
 
 	signal(SIGINT, controlc);
-	shell_name = argv[0];
+	shell_name = av[0];
 	while (1)
 	{
 		non_interactive();
@@ -37,19 +37,19 @@ int main(int argc __attribute__((unused)), char **argv)
 			deletec(line);
 			commands = tkn(line, ";");
 
-		for (i = 0; commands[i] != NULL; i++)
+		for (; commands[a] != NULL; a++)
 		{
-			current_command = tkn(commands[i], " ");
-			if (current_command[0] == NULL)
+			nowcmd = tkn(commands[a], " ");
+			if (nowcmd[0] == NULL)
 			{
-				free(current_command);
+				free(nowcmd);
 				break;
 			}
-			type_command = checkcmd(current_command[0]);
+			entercmd = checkcmd(nowcmd[0]);
 
 			/* initializer -   */
-			initializer(current_command, type_command);
-			free(current_command);
+			initializer(nowcmd, entercmd);
+			free(nowcmd);
 		}
 		free(commands);
 	}
